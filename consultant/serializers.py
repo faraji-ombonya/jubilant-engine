794b7de16f9b977c2eac8ff2fb8a6d1a76961575
Faraji Ombonya <farajiombonya@gmail.com>
2024-05-04 11:17:27 +0300

    feat: return boolean value for heart disease

--- consultant/serializers.py
+++ consultant/serializers.py
@@ -14,7 +14,7 @@ class AnswerSerializer(serializers.ModelSerializer):
         prediction = m.predict(numbers)
 
         ret = super().to_representation(instance)
-        ret['heart_disease'] = prediction[0]
+        ret['heart_disease'] = True if prediction[0] == "1" else False
 
         return ret
 
